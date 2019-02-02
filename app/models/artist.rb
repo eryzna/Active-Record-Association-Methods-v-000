66e44cf8310d4fc5a5cc47d8dac14e76271f4d57
Elana Ryznar <eryzna@gmail.com>
2019-02-02 21:53:25 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,16 +4,13 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     self.songs.first.genre
-    #return the genre of the artist's first saved song
   end
 
   def song_count
     self.songs.count
-    #return the number of songs associated with the artist
   end
 
   def genre_count
     self.genres.count
-    #return the number of genres associated with the artist
   end
 end
