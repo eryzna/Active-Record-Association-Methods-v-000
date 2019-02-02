27089ff0973b975d3d1a2424e1cc4104a43d14ca
Elana Ryznar <eryzna@gmail.com>
2019-02-02 21:53:09 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,12 +4,10 @@ class Genre < ActiveRecord::Base
 
   def song_count
     self.songs.count
-    # return the number of songs in a genre
   end
 
   def artist_count
     self.artists.count
-    # return the number of artists associated with the genre
   end
 
   def all_artist_names
@@ -18,6 +16,5 @@ class Genre < ActiveRecord::Base
       names << m.name
     end
     names
-    # return an array of strings containing every musician's name
   end
 end
